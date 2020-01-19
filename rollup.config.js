882da3d9032e91103c12575552e4db4afca179c0
Edonis Seferi <doni.seferi@gmail.com>
2020-01-19 22:04:02 +0000

    init

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+
+const config = {
+  input: "src/index.js",
+  output: [
+    {
+      file: "prayertimes.js",
+      name: pkg.name,
+      format: "umd",
+      exports: "named",
+      compact: true
+    }
+  ],
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    }),
+    terser()
+  ]
+};
+
+export default config;
