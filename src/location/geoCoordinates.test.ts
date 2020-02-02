7aa9c860bb04f8a4d7c85c6ae688f8241d8a48a4
Edonis Seferi <doni.seferi@gmail.com>
2020-02-02 21:38:23 +0000

    geoCoordinates refactored

--- src/location/geoCoordinates.test.ts
+++ src/location/geoCoordinates.test.ts
@@ -1,5 +1,6 @@
 import { geoCoordinate } from './index';
 import { iterativeTest } from '../testUtils/propertyTestHelper';
+import { degree } from '../maths';
 
 describe('geoCoordinates', () => {
     test('rejects a latitude value less than -90', () => {
@@ -7,7 +8,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: -90.01,
             maxInputValue: Number.MIN_SAFE_INTEGER,
-            assertion: value => expect(() => geoCoordinate(value, 10)).toThrow(RangeError)
+            assertion: value => expect(() => geoCoordinate(degree(value), degree(10))).toThrow(RangeError)
         });
     }),
     test('rejects a latitude value greater than 90', () => {
@@ -15,7 +16,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: 90.01,
             maxInputValue: Number.MAX_SAFE_INTEGER,
-            assertion: value => expect(() => geoCoordinate(value, 10)).toThrow(RangeError)
+            assertion: value => expect(() => geoCoordinate(degree(value), degree(10))).toThrow(RangeError)
         });
     }),
     test('rejects a longitude value less than -180', () => {
@@ -23,7 +24,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: -180.01,
             maxInputValue: Number.MIN_SAFE_INTEGER,
-            assertion: value => expect(() => geoCoordinate(10, value)).toThrow(RangeError)
+            assertion: value => expect(() => geoCoordinate(degree(10), degree(value))).toThrow(RangeError)
         });
     }),
     test('rejects a longitude value greater than 180', () => {
@@ -31,7 +32,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: 180.01,
             maxInputValue: Number.MAX_SAFE_INTEGER,
-            assertion: value => expect(() => geoCoordinate(10, value)).toThrow(RangeError)
+            assertion: value => expect(() => geoCoordinate(degree(10), degree(value))).toThrow(RangeError)
         });
     }),
     test('accept any latitude within the range of -90 to 90', () => {
@@ -39,7 +40,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: -90,
             maxInputValue: 90,
-            assertion: value => expect(() => geoCoordinate(value, 10)).not.toThrow()
+            assertion: value => expect(() => geoCoordinate(degree(value), degree(10))).not.toThrow()
         });
     }),
     test('accept any longitude within the range of -180 to 180', () => {
@@ -47,7 +48,7 @@ describe('geoCoordinates', () => {
             numberOfExecutions: 500,
             minInputValue: -180,
             maxInputValue: 180,
-            assertion: value => expect(() => geoCoordinate(10, value)).not.toThrow()
+            assertion: value => expect(() => geoCoordinate(degree(10), degree(value))).not.toThrow()
         });
     })
 });
