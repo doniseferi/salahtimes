1b25aa4cd430b99969a46ddb702fa8c32000bd1e
doniseferi <doni.seferi@gmail.com>
2020-02-05 20:37:55 +0000

    Cdebug (#2)
    * travis updated
    
    * removed coverage
    
    * travis update
    
    * exporting salahtimesobject
    
    * removed unnes exports
    
    * debug
    
    * travis update
    
    * travis update
    
    * whitespace fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import {
     geoCoordinate,
-    GeoCoordinate
 } from './location/index'
 import {
     degree,
-    Degree
 } from './maths/index';
 
 export {
     geoCoordinate,
-    GeoCoordinate,
     degree,
-    Degree
 }
