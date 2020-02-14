c1a92bc022a9ee01dfcbce6f4b29b826bbe4cee2
Edonis Seferi <edonis.seferi@asos.com>
2020-02-14 16:20:48 +0000

    timespan changes

--- src/time/__tests__/timeSpan.test.ts
+++ src/time/__tests__/timeSpan.test.ts
@@ -31,54 +31,38 @@ const generateEqualItem = (): TimeSpanTestSubject => {
   return createTimeSPanTestSubject(date, date);
 };
 
-describe("TimeSpan", () => {
-  test("returns the time span in milliseconds between two dates", () => {
+describe("TimeSpan value", () => {
+  test("is in miniseconds", () => {
     iterativeTest<TimeSpanTestSubject, void>({
       numberOfExecutions: 500,
       generateInput: () => generateRandomItem(),
       assert: input => expect(input.actual).toEqual(input.expect)
     });
   }),
-    test("returns a positive value", () => {
+    test("returns a positive value when param item 1 is later than param item 2", () => {
       iterativeTest<TimeSpanTestSubject, void>({
         numberOfExecutions: 500,
         generateInput: () => generatePositiveItem(),
         assert: input => expect(input.actual).toBeGreaterThan(0)
       });
     }),
-    test("returns a negative value", () => {
+    test("returns a negative value when param item 1 is earlier than param item 2", () => {
       iterativeTest<TimeSpanTestSubject, void>({
         numberOfExecutions: 500,
         generateInput: () => generateNegativeItem(),
         assert: input => expect(input.actual).toBeLessThan(0)
       });
     }),
-    test("returns a an equal value", () => {
+    test("returns a an equal value when both param items are the same date", () => {
       iterativeTest<TimeSpanTestSubject, void>({
         numberOfExecutions: 500,
         generateInput: () => generateEqualItem(),
         assert: input => expect(input.actual).toEqual(0)
       });
-    }),
-    test("returns a positive value when an earlier date is subtracted for a later date", () => {
-      const A = new Date(1987, 0, 27, 12, 0, 0, 0);
-      const B = new Date(1987, 0, 27, 12, 0, 0, 1);
-      const span = timeSpan(B, A);
-      const expectedSpanInMilliSeconds = 1;
-      expect(span.value).toEqual(expectedSpanInMilliSeconds);
-    }),
-    test("returns a negative value when a later date is subtracted for am earlier date", () => {
-      const A = new Date(1987, 0, 27, 12, 0, 0, 1);
-      const B = new Date(1987, 0, 27, 12, 0, 0, 0);
-      const span = timeSpan(B, A);
-      const expectedSpanInMilliSeconds = -1;
-      expect(span.value).toEqual(expectedSpanInMilliSeconds);
-    }),
-    test("returns a 0 value when a two dates are equal", () => {
-      const A = new Date(1987, 0, 27, 12, 0, 0, 0);
-      const B = new Date(1987, 0, 27, 12, 0, 0, 0);
-      const span = timeSpan(B, A);
-      const expectedSpanInMilliSeconds = 0;
-      expect(span.value).toEqual(expectedSpanInMilliSeconds);
-    });
+    })
 });
+describe("Timespan divide", () => {
+    test("", () => {
+
+    }),
+})
--- src/time/timeSpan.ts
+++ src/time/timeSpan.ts
@@ -1,15 +1,27 @@
 export interface TimeSpan {
     // divide(divisor: TimeSpan): TimeSpan
-    // divide(divisor: number): TimeSpan,
+    // divide(divisor: : number): TimeSpan,
     // duration(): Duration,
-    value: number
+    milliseconds: number
 }
 
-const timeSpan = (dateA: Date, dateB: Date): TimeSpan => {
-    const value = dateA.getTime() - dateB.getTime();
-    return {
-        value
-    }
-}
+const totalMilliseconds = (days: number, hours: number, minutes: number, seconds: number, milliseconds: number) => (days * 3600 * 24 + hours * 3600 + minutes * 60 + seconds) * 1000 + milliseconds;
+const timeSpan = (
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number,
+  milliseconds: number
+) => ({
+  milliseconds: totalMilliseconds(days, hours, minutes, seconds, milliseconds)
+});
+const timeSpan2 = (
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number
+) => timeSpan(days, hours, minutes, seconds, 0);
+const timeSpan3 = (hours: number, minutes: number, seconds: number) =>
+  timeSpan(0, hours, minutes, seconds, 0);
 
-export { timeSpan };
+  export { timeSpan, timeSpan2, timeSpan3 };
