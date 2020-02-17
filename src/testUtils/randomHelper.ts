7c460b9cecb271ab02fbc835e263d282cdd76977
Edonis Seferi <doni.seferi@gmail.com>
2020-02-17 20:08:10 +0000

    added missing semicolon

--- src/testUtils/randomHelper.ts
+++ src/testUtils/randomHelper.ts
@@ -10,7 +10,7 @@ const generateRandomDate = (minYear: number, maxYear: number): Date => {
     const startDate = new Date(minYear, 0, 1, 0, 0, 0, 0);
     const endDate = new Date(maxYear, 11, 31, 23, 59, 59, 999);
     return new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
-}
+};
 
 export {
     generateRandomNumber,
