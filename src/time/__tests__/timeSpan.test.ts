9acd2e1aee631ee5a904dbd12c9eec6b22929f5c
Edonis Seferi <edonis.seferi@asos.com>
2020-02-17 14:29:45 +0000

    division added

--- src/time/__tests__/timeSpan.test.ts
+++ src/time/__tests__/timeSpan.test.ts
@@ -1,50 +1,99 @@
 import { timeSpan, TimeSpan } from "../index";
-import { iterativeTest, generateRandomWholeNumber } from "../../testUtils/index";
+import {
+  iterativeTest,
+  generateRandomWholeNumber
+} from "../../testUtils/index";
 
 interface testSpec {
-    milliSecond: number,
-    span: TimeSpan
+  expected: number;
+  actual: TimeSpan;
 }
 
-const staticMillisecondValues = (
-    val: number,
-    arithmatic: (milisecconds: number, target: number) => number): testSpec[] => ([
-        {
-            milliSecond: arithmatic(86400000, val),
-            span: timeSpan(val, 0, 0, 0, 0)
-        },
-        {
-            milliSecond: arithmatic(3600000, val),
-            span: timeSpan(0, val, 0, 0, 0)
-        },
-        {
-            milliSecond: arithmatic(60000, val),
-            span: timeSpan(0, 0, val, 0, 0)
-        },
-        {
-            milliSecond: arithmatic(1000, val),
-            span: timeSpan(0, 0, 0, val, 0)
-        },
-        {
-            milliSecond: arithmatic(1, val),
-            span: timeSpan(0, 0, 0, 0, val)
-        }]);
+const timeSpanUnits = {
+  day: 86400000,
+  hour: 3600000,
+  minute: 60000,
+  second: 1000,
+  millisecond: 1
+};
 
-describe('TimeSpan', () => {
-    test('returns a value in millisecond', () => {
-        iterativeTest<testSpec[], void>({
-            numberOfExecutions: 500,
-            generateInput: () =>
-                staticMillisecondValues(
-                    generateRandomWholeNumber(
-                        -1_000_000_000_000, 1_000_000_000_000),
-                    (a, b) => a * b),
-            assert: testSpec => {
-                Object
-                    .values(testSpec)
-                    .forEach(timeSpan =>
-                        expect(timeSpan.span.value).toEqual(timeSpan.milliSecond))
-            }
-        })
-    })
+const timespanValueResult = (value: number): testSpec[] => [
+  {
+    expected: timeSpanUnits.day * value,
+    actual: timeSpan(value, 0, 0, 0, 0)
+  },
+  {
+    expected: timeSpanUnits.hour * value,
+    actual: timeSpan(0, value, 0, 0, 0)
+  },
+  {
+    expected: timeSpanUnits.minute * value,
+    actual: timeSpan(0, 0, value, 0, 0)
+  },
+  {
+    expected: timeSpanUnits.second * value,
+    actual: timeSpan(0, 0, 0, value, 0)
+  },
+  {
+    expected: timeSpanUnits.millisecond * value,
+    actual: timeSpan(0, 0, 0, 0, value)
+  }
+];
+
+const timespanDivisioNResult = (
+  initalValue: number,
+  divisor: number
+): testSpec[] => [
+  {
+    expected: ((timeSpanUnits.day * initalValue) / divisor) >> 0,
+    actual: timeSpan(initalValue, 0, 0, 0, 0).divide(divisor)
+  },
+  {
+    expected: ((timeSpanUnits.hour * initalValue) / divisor) >> 0,
+    actual: timeSpan(0, initalValue, 0, 0, 0).divide(divisor)
+  },
+  {
+    expected: ((timeSpanUnits.minute * initalValue) / divisor) >> 0,
+    actual: timeSpan(0, 0, initalValue, 0, 0).divide(divisor)
+  },
+  {
+    expected: ((timeSpanUnits.second * initalValue) / divisor) >> 0,
+    actual: timeSpan(0, 0, 0, initalValue, 0).divide(divisor)
+  },
+  {
+    expected: ((timeSpanUnits.millisecond * initalValue) / divisor) >> 0,
+    actual: timeSpan(0, 0, 0, 0, initalValue).divide(divisor)
+  }
+];
+
+describe("TimeSpan", () => {
+  test("returns a value in millisecond", () => {
+    iterativeTest<testSpec[], void>({
+      numberOfExecutions: 500,
+      generateInput: () =>
+        timespanValueResult(
+          generateRandomWholeNumber(-1000000000000, 1000000000000)
+        ),
+      assert: testSpec => {
+        Object.values(testSpec).forEach(timeSpan =>
+          expect(timeSpan.actual.value).toEqual(timeSpan.expected)
+        );
+      }
+    });
+  });
+  test("is divisible", () => {
+    iterativeTest<testSpec[], void>({
+      numberOfExecutions: 500,
+      generateInput: () =>
+        timespanDivisioNResult(
+          generateRandomWholeNumber(-1_000_000_000_000, 1_000_000_000_000),
+          generateRandomWholeNumber(-1_000_000_000_000, 1_000_000_000_000)
+        ),
+      assert: testSpec => {
+        Object.values(testSpec).forEach(timeSpan =>
+          expect(timeSpan.actual.value).toEqual(timeSpan.expected)
+        );
+      }
+    });
+  });
 });
--- src/time/timeSpan.ts
+++ src/time/timeSpan.ts
@@ -1,10 +1,18 @@
 export interface TimeSpan {
   // divide(divisor: TimeSpan): TimeSpan
-  // divide(divisor: : number): TimeSpan,
-  value: number
+  divide(divisor: number): TimeSpan,
+  value: number;
 }
 
-const calculateMilliseconds = (days: number, hours: number, minutes: number, seconds: number, milliseconds: number) => (days * 3600 * 24 + hours * 3600 + minutes * 60 + seconds) * 1000 + milliseconds;
+const calculateMilliseconds = (
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number,
+  milliseconds: number
+) =>
+  (days * 3600 * 24 + hours * 3600 + minutes * 60 + seconds) * 1000 +
+  milliseconds;
 const timeSpan = (
   days: number,
   hours: number,
@@ -12,7 +20,14 @@ const timeSpan = (
   seconds: number,
   milliseconds: number
 ): TimeSpan => ({
-  value: calculateMilliseconds(days, hours, minutes, seconds, milliseconds)
+  value: calculateMilliseconds(days, hours, minutes, seconds, milliseconds),
+  divide: (divisor: number) => timeSpan(0,0,0,0, ~~(calculateMilliseconds(
+      days,
+      hours,
+      minutes,
+      seconds,
+      milliseconds
+    ) / divisor))
 });
 
 export { timeSpan };
