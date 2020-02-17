6e822176316f645398b95a49041abc36f1ecb59c
Edonis Seferi <doni.seferi@gmail.com>
2020-02-17 20:22:15 +0000

    skeletal checkin

---
+++ src/location/highLatitude/__tests__/highLatitude.test.ts
@@ -0,0 +1,12 @@
+describe('High latitude calclulation method',()=>{
+    test('Angle based method returns', () => {
+        expect(false).toBeTruthy();
+    });
+});
+
+/*
+Let α be the twilight angle for Isha.
+Let t = α/60.
+Let x = the time span between sunset and sunrise.
+Let n = x / t
+/*
---
+++ src/location/highLatitude/angleBasedMethod.ts
@@ -0,0 +1,6 @@
+import { Degree } from "../../maths";
+import { timeSpan } from "../../time";
+
+export default (angle: Degree) => {
+    return timeSpan(0,0,0,0,1);
+};
