3d9b1bab740fcfee787e7cf7d502616a2a0cc457
Edonis Seferi <doni.seferi@gmail.com>
2020-02-17 19:50:49 +0000

    refactor based on codecy

--- src/time/timeSpan.ts
+++ src/time/timeSpan.ts
@@ -1,20 +1,11 @@
+import calculateMilliseconds from './calculateMilliseconds';
+
 export interface TimeSpan {
   divide(divisor: number): TimeSpan;
   divideByTimeSpan(divisor: TimeSpan): TimeSpan;
   value: number;
-}
+};
 
-const millisecond = (value: number) => timeSpan(0, 0, 0, 0, value);
-
-const calculateMilliseconds = (
-  days: number,
-  hours: number,
-  minutes: number,
-  seconds: number,
-  milliseconds: number
-) =>
-  (days * 3600 * 24 + hours * 3600 + minutes * 60 + seconds) * 1000 +
-  milliseconds;
 const timeSpan = (
   days: number,
   hours: number,
@@ -24,19 +15,21 @@ const timeSpan = (
 ): TimeSpan => ({
   value: calculateMilliseconds(days, hours, minutes, seconds, milliseconds),
   divide: (divisor: number) =>
-    millisecond(
+    fromMilliseconds(
       ~~(
         calculateMilliseconds(days, hours, minutes, seconds, milliseconds) /
         divisor
       )
     ),
   divideByTimeSpan: (divisor: TimeSpan) =>
-    millisecond(
+    fromMilliseconds(
       ~~(
         calculateMilliseconds(days, hours, minutes, seconds, milliseconds) /
         divisor.value
       )
     )
 });
+
+const fromMilliseconds = (value: number) => timeSpan(0, 0, 0, 0, value)
 
 export { timeSpan };
