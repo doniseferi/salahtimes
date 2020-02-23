361097660dbae669e7c3975c66dc192e9fbbfd85
Edonis Seferi <doni.seferi@gmail.com>
2020-02-23 22:07:13 +0000

    test fixes

--- src/location/highLatitude/__tests__/highLatitude.test.ts
+++ src/location/highLatitude/__tests__/highLatitude.test.ts
@@ -1,7 +1,7 @@
 import { angleBasedMethod, oneSeventhMethod, middleOfTheNightMethod } from '../../index';
-import { TimeSpan, timeSpan } from '../../../time/index';
+import { timeSpan, TimeSpan } from '../../../time/index';
 import { degree, Degree } from '../../../maths/index';
-import { Either, matchOrThrow } from "../../../either/index";
+import { matchOrThrow } from '../../../either/index';
 import { iterativeTest, randomTimeSpan, randomDegree, generateRandomWholeNumber } from '../../../testUtils/index'
 
 describe('High latitude: Angle based method  pre conditions', () => {
@@ -63,13 +63,13 @@ describe('High latitude calculation post conditions', () => {
       numberOfExecutions: 500,
       generateInput: () => highLatitudeTestSpec(matchOrThrow(degree(generateRandomWholeNumber(1, 90))), timeSpan(0, generateRandomWholeNumber(0, 23), 0, 0, 0)),
       assert: (val) => {
-        const expected = (val.timeSpanBetweenSunsetAndSunrise.value / val.angle.value) >> 0;
-        const actual = matchOrThrow(val.actual).value;
-        expect(actual).toEqual(expected);
+        const expected = (val.timeSpanBetweenSunsetAndSunrise.value / val.angle) >> 0;
+        const actual = val.actual;
+        expect(actual.value).toEqual(expected);
       }
     });
   });
-  test('Angle based method The result will always return a time span', () => {
+  test('the result will always return a time span', () => {
     iterativeTest<HighLatitudeTestSpec, void>({
       numberOfExecutions: 500,
       generateInput: () =>
@@ -77,15 +77,15 @@ describe('High latitude calculation post conditions', () => {
           matchOrThrow(degree(generateRandomWholeNumber(1, 90))),
           timeSpan(0, generateRandomWholeNumber(0, 23), 0, 0, 0)
         ),
-      assert: val => {
-        expect(matchOrThrow(val.actual)).toMatchObject<TimeSpan>(matchOrThrow(val.actual));
+      assert: testData => {
+        expect(testData.actual as TimeSpan).not.toBeNull();
       }
     });
   });
 });
 
-describe("High latitude: One seventh method pre conditions", () => {
-  test("throws an error when no time span is null", () => {
+describe('High latitude: One seventh method pre conditions', () => {
+  test('throws an error when no time span is null', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => (null as unknown) as TimeSpan,
@@ -94,7 +94,7 @@ describe("High latitude: One seventh method pre conditions", () => {
       }
     });
   });
-  test("throws an error when no time span is undefined", () => {
+  test('throws an error when no time span is undefined', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => undefined as unknown as TimeSpan,
@@ -118,8 +118,8 @@ describe('High latitude: One seventh based method invariance', () => {
     });
   });
 });
-describe("High latitude: One seventh based method  post conditions", () => {
-  test("The result will always equal the time span divided by 7", () => {
+describe('High latitude: One seventh based method  post conditions', () => {
+  test('The result will always equal the time span divided by 7', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => randomTimeSpan(),
@@ -130,22 +130,19 @@ describe("High latitude: One seventh based method  post conditions", () => {
       }
     });
   });
-  test("the result will always return a time span", () => {
+  test('the result will always return a time span', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => randomTimeSpan(),
       assert: (val) => {
-        const expectedMilliseconds = ((val.value / 7) >> 0);
-        const expected = timeSpan(0, 0, 0, 0, expectedMilliseconds);
-        const result = matchOrThrow(oneSeventhMethod(val));
-        expect(result).toEqual(expected);
+        expect(matchOrThrow(oneSeventhMethod(val)) as TimeSpan).not.toBeNull();
       }
     });
   });
 });
 
-describe("High latitude: Middle of the night method pre conditions", () => {
-  test("throws an error when no time span is null", () => {
+describe('High latitude: Middle of the night method pre conditions', () => {
+  test('throws an error when no time span is null', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => (null as unknown) as TimeSpan,
@@ -154,7 +151,7 @@ describe("High latitude: Middle of the night method pre conditions", () => {
       }
     });
   });
-  test("throws an error when no time span is undefined", () => {
+  test('throws an error when no time span is undefined', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => undefined as unknown as TimeSpan,
@@ -178,8 +175,8 @@ describe('High latitude: Middle of the night based method invariance', () => {
     });
   });
 });
-describe("High latitude: Middle of the night based method  post conditions", () => {
-  test("The result will always equal the time span divided by 4", () => {
+describe('High latitude: Middle of the night based method  post conditions', () => {
+  test('The result will always equal the time span divided by 4', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => randomTimeSpan(),
@@ -190,14 +187,12 @@ describe("High latitude: Middle of the night based method  post conditions", () 
       }
     });
   });
-  test("the result will always return a time span", () => {
+  test('the result will always return a time span', () => {
     iterativeTest<TimeSpan, void>({
       numberOfExecutions: 500,
       generateInput: () => randomTimeSpan(),
       assert: (val) => {
-        const actual = matchOrThrow(middleOfTheNightMethod(val));
-        const expected = timeSpan(0, 0, 0, 0, (val.value / 4 >> 0));
-        expect(actual).toEqual(expected);
+        expect(matchOrThrow(middleOfTheNightMethod(val)) as TimeSpan).not.toBeNull();
       }
     });
   });
@@ -205,12 +200,12 @@ describe("High latitude: Middle of the night based method  post conditions", () 
 
 interface HighLatitudeTestSpec {
   timeSpanBetweenSunsetAndSunrise: TimeSpan,
-  angle: Degree,
-  actual: Either<Error, TimeSpan>
+  angle: number,
+  actual: Readonly<TimeSpan>
 };
 
 const highLatitudeTestSpec = (degree: Degree, timeSpanBetweenSunsetAndSunrise: TimeSpan): HighLatitudeTestSpec => ({
   timeSpanBetweenSunsetAndSunrise,
-  angle: degree,
-  actual: angleBasedMethod(degree, timeSpanBetweenSunsetAndSunrise)
+  angle: degree.value,
+  actual: matchOrThrow(angleBasedMethod(degree, timeSpanBetweenSunsetAndSunrise))
 });
