26215f69370f9d60e08b487361b32976f8098843
Edonis Seferi <edonis.seferi@asos.com>
2020-02-26 13:34:55 +0000

    refactored trig

--- src/maths/trigonometry.ts
+++ src/maths/trigonometry.ts
@@ -1,11 +1,8 @@
-import { Degree, degree } from ".";
-import { Either, match } from "../either";
+import { Degree, degree } from '.'
+import { Either, match } from '../either'
 
 const radiansToDegrees = (radians: number): Either<RangeError, Degree> => degree(radians * (180 / Math.PI))
 
-const arccot = (degree: Readonly<Degree>): number => {
-    const sumkking = Math.PI / 2 - Math.atan(degree.value);
-    return match(radiansToDegrees(sumkking), (err) => {throw err}, (deg) => deg.value)
-}
+const arccot = (degree: Readonly<Degree>): number => match(radiansToDegrees(Math.PI / 2 - Math.atan(degree.value)), (err) => { throw err }, (deg) => deg.value)
 
 export { arccot }
