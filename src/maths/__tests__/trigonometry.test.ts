aa38396ad45f171ce7019296c4f1afa4a449cc58
Edonis Seferi <edonis.seferi@asos.com>
2020-03-04 17:54:57 +0000

    arccot added

--- src/maths/__tests__/trigonometry.test.ts
+++ src/maths/__tests__/trigonometry.test.ts
@@ -1,11 +1,11 @@
-import { tan, Degree } from '..'
+import { degreesToRadians, arccot, tan, Degree } from '..'
 import { matchOrThrow } from '../../either'
 import { iterativeTest, randomDegree } from '../../testUtils'
-import { degreesToRadians } from '../trigonometry'
+import { radiansToDegrees } from '../trigonometry'
 
 describe('Tangent', () => {
   test('throws an error when the angular degrees object is null', () => {
-    expect(() => tan(null as unknown as Degree)).toThrow();
+    expect(() => tan(null as unknown as Degree)).toThrow()
   })
   test('returns the tangent for angular degrees', () => {
     iterativeTest({
@@ -15,7 +15,32 @@ describe('Tangent', () => {
         const angleInRadians = matchOrThrow(degreesToRadians(degrees))
         const expected = Math.tan(angleInRadians)
         const actual = tan(degrees)
-        expect(actual).toEqual(expected)
+        const EPSILON = 0.0001
+        const diff = Math.abs(actual - expected)
+        const max = Math.max(actual, expected)
+        const maxDiff = Math.abs(max * EPSILON)
+        const inAcceptableRange = diff <= maxDiff
+        console.log({ diff, max, maxDiff })
+        expect(inAcceptableRange).toEqual(true)
+      }
+    })
+  })
+})
+
+describe('Inverse Cotangent', () => {
+  test('throws an error when the angular degrees object is null', () => {
+    expect(() => arccot(null as unknown as Degree)).toThrow()
+  })
+  test('returns the inverse cotangent for angular degrees', () => {
+    iterativeTest({
+      numberOfExecutions: 500,
+      generateInput: () => randomDegree(-360, 360),
+      assert: (degrees) => {
+        const angleInRadians = matchOrThrow(degreesToRadians(degrees))
+        const expected = Math.atan(1 / angleInRadians)
+        const expectedInDegrees = radiansToDegrees(expected)
+        const actual = arccot(degrees)
+        expect(actual).toEqual(expectedInDegrees)
       }
     })
   })
--- src/maths/trigonometry.ts
+++ src/maths/trigonometry.ts
@@ -1,5 +1,5 @@
 import { Degree, degree } from '.'
-import { Either, match, left, right } from '../either'
+import { Either, match, left, right, matchOrThrow } from '../either'
 
 const radiansToDegrees = (radians: number): Either<RangeError, Degree> => degree(radians * (180 / Math.PI))
 const degreesToRadians = (degrees: Readonly<Degree>): Either<Error, number> =>
@@ -13,12 +13,12 @@ const arccot = (degrees: Readonly<Degree>): Either<Error, Readonly<Degree>> =>
     ? left(new Error('The angular degrees is null or empty.'))
     : right(
       match(
-        degree(Math.PI / 2 - Math.atan(degrees.value)),
+        radiansToDegrees(Math.atan(1 / matchOrThrow(degreesToRadians(degrees)))),
         (err) => { throw err },
         (deg) => deg))
 
 const tan = (degrees: Readonly<Degree>): number =>
-  Math.tan(
+  Math.atan(
     match(
       degreesToRadians(degrees),
       (err) => { throw err },
