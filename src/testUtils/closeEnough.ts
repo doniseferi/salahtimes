2e5ad14d2bc7af2991f948b8e4086978e3663faf
Edonis Seferi <edonis.seferi@asos.com>
2020-03-18 21:36:59 +0000

    removed console.log

--- src/testUtils/closeEnough.ts
+++ src/testUtils/closeEnough.ts
@@ -12,11 +12,7 @@ export default (x: number, y: number, epsilon: number = 1.1e-13): boolean => {
     if (diff < Number.EPSILON) {
       return true
     } else {
-      const result = diff <= Math.max(Math.abs(x), Math.abs(y)) * epsilon
-      if (!result) {
-        console.log({ result, x, y })
-      }
-      return result
+      return diff <= Math.max(Math.abs(x), Math.abs(y)) * epsilon
     }
   }
 
