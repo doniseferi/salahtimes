51c9e5e15cd8aaafbd4817a69a95a7deec6e04e9
Edonis Seferi <edonis.seferi@asos.com>
2020-03-18 17:09:42 +0000

    fajr test update

--- src/fajr/__tests__/fajr.test.ts
+++ src/fajr/__tests__/fajr.test.ts
@@ -1,7 +1,7 @@
 import fajr from '../'
 import { convention, Convention } from '../../convention'
 import { geoCoordinate, latitude, longitude } from '../../location'
-import { throwOnError, success } from '../../either'
+import { throwOnError } from '../../either'
 import { closeEnough } from '../../testUtils'
 
 describe('Fajr', () => {
@@ -38,98 +38,92 @@ describe('Fajr', () => {
         convention(),
         Date.UTC(2025, 11, 31, 15, 13, 38, 499)))
       .toEqual(true)
-    expect(
-      fajr(
-        new Date(2026, 0, 1),
-        geoCoordinate(
-          throwOnError(latitude(-36.8484597)),
-          throwOnError(longitude(174.7633315))),
-        convention()))
-      .toEqual(success(new Date(Date.UTC(2025, 11, 31, 15, 13, 38, 499))))
   })
 })
+
 describe('Conventions', () => {
   test('Default (Muslim World League) convention returns the correct fajr date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention()))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 14, 18, 672))))
+        21.3890824,
+        39.8579118,
+        convention(),
+        Date.UTC(2021, 5, 1, 1, 14, 18, 672)))
+      .toEqual(true)
   })
   test('Muslim World League convention returns the correct fajr date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention()))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 14, 18, 672))))
+        21.3890824,
+        39.8579118,
+        convention(),
+        Date.UTC(2021, 5, 1, 1, 14, 18, 672)))
+      .toEqual(true)
   })
   test('Egyptian General Authority Of Survey convention returns the correct fajr date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('EgyptianGeneralAuthorityOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 6, 33, 422))))
+        21.3890824,
+        39.8579118,
+        convention('EgyptianGeneralAuthorityOfSurvey'),
+        Date.UTC(2021, 5, 1, 1, 6, 33, 422)))
+      .toEqual(true)
   })
   test('Institute Of Geophysics University Of Tehran Of Survey convention returns the correct ishaa date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('InstituteOfGeophysicsUniversityOfTehranOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 15, 51, 54))))
+        21.3890824,
+        39.8579118,
+        convention('InstituteOfGeophysicsUniversityOfTehranOfSurvey'),
+        Date.UTC(2021, 5, 1, 1, 15, 51, 54)))
+      .toEqual(true)
   })
   test('Islamic Society Of North America convention returns the correct ishaa date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('IslamicSocietyOfNorthAmerica')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 29, 33, 404))))
+        21.3890824,
+        39.8579118,
+        convention('IslamicSocietyOfNorthAmerica'),
+        Date.UTC(2021, 5, 1, 1, 29, 33, 404)))
+      .toEqual(true)
   })
   test('Shia Ithna Ashari Leva Research Institute Qum Of Survey convention returns the correct ishaa date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('ShiaIthnaAshariLevaResearchInstituteQumOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 24, 30, 659))))
+        21.3890824,
+        39.8579118,
+        convention('ShiaIthnaAshariLevaResearchInstituteQumOfSurvey'),
+        Date.UTC(2021, 5, 1, 1, 24, 30, 659)))
+      .toEqual(true)
   })
   test('Umm Al Qura University, Mekkah convention returns the correct ishaa date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('UmmAlQuraUniversityMekkah')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 11, 44, 218))))
+        21.3890824,
+        39.8579118,
+        convention('UmmAlQuraUniversityMekkah'),
+        Date.UTC(2021, 5, 1, 1, 11, 44, 218)))
+      .toEqual(true)
   })
   test('University Of Islamic Sciences, Karachi convention returns the correct ishaa date time utc', () => {
     expect(
-      fajr(
+      isFajrDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('UniversityOfIslamicSciencesKarachi')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 1, 14, 18, 672))))
+        21.3890824,
+        39.8579118,
+        convention('UniversityOfIslamicSciencesKarachi'),
+        Date.UTC(2021, 5, 1, 1, 14, 18, 672)))
+      .toEqual(true)
   })
 })
+
 const isFajrDateTimeUtcCloseEnough =
   (date: Date,
     lat: number,
