c741b8f1f38622d011dfcfef2f7b188e05c77155
Edonis Seferi <edonis.seferi@asos.com>
2020-03-18 18:05:07 +0000

    ishaa tests

--- src/ishaa/__test__/ishaa.test.ts
+++ src/ishaa/__test__/ishaa.test.ts
@@ -1,87 +1,98 @@
 import ishaa from '../'
-import { convention } from '../../convention'
+import { convention, Convention } from '../../convention'
 import { geoCoordinate, latitude, longitude } from '../../location'
-import { throwOnError, success } from '../../either'
+import { throwOnError } from '../../either'
+import { closeEnough } from '../../testUtils'
 
 describe('Conventions', () => {
   test('Default (Muslim World League) convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention()))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 17, 18, 565))))
+        21.3890824,
+        39.8579118,
+        convention(),
+        Date.UTC(2021, 5, 1, 17, 17, 18, 565))).toEqual(true)
   })
   test('Muslim World League convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention()))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 17, 18, 565))))
+        21.3890824,
+        39.8579118,
+        convention(),
+        Date.UTC(2021, 5, 1, 17, 17, 18, 565))).toEqual(true)
   })
   test('Egyptian General Authority Of Survey convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('EgyptianGeneralAuthorityOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 19, 51, 835))))
+        21.3890824,
+        39.8579118,
+        convention('EgyptianGeneralAuthorityOfSurvey'),
+        Date.UTC(2021, 5, 1, 17, 19, 51, 835))).toEqual(true)
   })
   test('Institute Of Geophysics University Of Tehran Of Survey convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('InstituteOfGeophysicsUniversityOfTehranOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 2, 10, 202))))
+        21.3890824,
+        39.8579118,
+        convention('InstituteOfGeophysicsUniversityOfTehranOfSurvey'),
+        Date.UTC(2021, 5, 1, 17, 2, 10, 202))).toEqual(true)
   })
   test('Islamic Society Of North America convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('IslamicSocietyOfNorthAmerica')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 7, 10, 955))))
+        21.3890824,
+        39.8579118,
+        convention('IslamicSocietyOfNorthAmerica'),
+        Date.UTC(2021, 5, 1, 17, 7, 10, 955))).toEqual(true)
   })
   test('Shia Ithna Ashari Leva Research Institute Qum Of Survey convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('ShiaIthnaAshariLevaResearchInstituteQumOfSurvey')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 2, 10, 202))))
+        21.3890824,
+        39.8579118,
+        convention('ShiaIthnaAshariLevaResearchInstituteQumOfSurvey'),
+        Date.UTC(2021, 5, 1, 17, 2, 10, 202))).toEqual(true)
   })
   test('Umm Al Qura University, Mekkah convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('UmmAlQuraUniversityMekkah')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 46, 0, 105))))
+        21.3890824,
+        39.8579118,
+        convention('UmmAlQuraUniversityMekkah'),
+        Date.UTC(2021, 5, 1, 17, 46, 0, 105))).toEqual(true)
   })
   test('University Of Islamic Sciences, Karachi convention returns the correct ishaa date time utc', () => {
     expect(
-      ishaa(
+      isIshaaDateTimeUtcCloseEnough(
         new Date(2021, 5, 1),
-        geoCoordinate(
-          throwOnError(latitude(21.3890824)),
-          throwOnError(longitude(39.8579118))),
-        convention('UniversityOfIslamicSciencesKarachi')))
-      .toEqual(success(new Date(Date.UTC(2021, 5, 1, 17, 22, 25, 687))))
+        21.3890824,
+        39.8579118,
+        convention('UniversityOfIslamicSciencesKarachi'),
+        Date.UTC(2021, 5, 1, 17, 22, 25, 687))).toEqual(true)
   })
 })
+
+const isIshaaDateTimeUtcCloseEnough =
+  (date: Date,
+    lat: number,
+    lng: number,
+    ishaaConvention: Convention,
+    expectedDateUtc: number):
+  boolean =>
+    closeEnough(
+      new Date(throwOnError(
+        ishaa(
+          date,
+          geoCoordinate(
+            throwOnError(latitude(lat)),
+            throwOnError(longitude(lng))),
+          ishaaConvention)))
+        .getTime(),
+      expectedDateUtc)
